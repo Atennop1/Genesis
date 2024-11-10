ad31743850398d89097aece540eb5855ca817dc1
AtennopVer <anton7185569@gmail.com>
2024-11-10 22:29:46 +0300

    Made INSTALLATION.md guide

--- src/shortcuts/shortcuts.cpp
+++ src/shortcuts/shortcuts.cpp
@@ -30,18 +30,20 @@ SDL_FRect Shortcuts::PositionAndSizeToRect(SDL_Window* window, Vector2 position,
 
 SDL_FRect Shortcuts::PositionAndTextureToRect(SDL_Window* window, Vector2 position, SDL_Texture *texture, float position_multiplier, float size_multiplier)
 {
-    int width = 0;
-    int height = 0;
+    // 70 166
+
+    int width = 0; // 15
+    int height = 0; // 7
     SDL_FRect rect = { };
     SDL_QueryTexture(texture, nullptr, nullptr, &width, &height);
 
-    rect.h = (float)height * size_multiplier;
-    rect.w = (float)width * size_multiplier;
-    rect.x = position.x * position_multiplier - rect.w / 2.0f;
+    rect.h = (float)height * size_multiplier; //42
+    rect.w = (float)width * size_multiplier; // 60
+    rect.x = position.x * position_multiplier - rect.w / 2.0f; // 390
 
-    int window_height = 0;
+    int window_height = 0; //1080
     SDL_GetWindowSize(window, NULL, &window_height);
-    rect.y = window_height - position.y * position_multiplier - rect.h / 2.0f;
+    rect.y = window_height - position.y * position_multiplier - rect.h / 2.0f; // 63
     return rect;
 }
 }
